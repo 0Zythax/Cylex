f0849c55fd382ea28c1418776d0842ef521d648b
0zythax <zythax@tutamail.com>
2025-09-14 21:15:35 +0100

    more shit

---
+++ routes/home.js
@@ -0,0 +1,8 @@
+// yh
+
+const express = require("express")
+const router = express.Router()
+const protectedPage = require("../middleware/authorization.js")
+
+//router.get("/", protectedPage)
+module.exports = router
--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,10 @@ const express = require("express")
 const router = express.Router()
 
 const loginRoute = require("../routes/login.js")
+const homeRoute = require("../routes/home.js")
 const pageNotFound = require("../middleware/pageNotFound.js")
 
+router.use("/", homeRoute)
 router.use("/login", loginRoute)
 router.use(express.static("public"))
 router.use(pageNotFound)
